57f23c7944058e700a860c1d1b7d42bbbd2e0fb9
Avery Oliver <averyaoliver03@gmail.com>
2024-02-27 22:25:10 -0600

    COMPLETED

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -1,271 +1,157 @@
-require "spec_helper"
-
-require_relative "../lib/gilded_rose"
-
 RSpec.describe GildedRose do
-  context "Normal Item" do
-    it "before sell date" do
-      gilded_rose = GildedRose.new(name: "Normal Item", days_remaining: 5, quality: 10)
+  describe "#tick" do
+    context "Normal Item" do
+      context "before sell date" do
+        it "reduces the days remaining and quality" do
+          gilded_rose = GildedRose.new(name: "Normal Item", days_remaining: 5, quality: 10)
+          gilded_rose.tick
+          expect(gilded_rose).to have_attributes(days_remaining: 4, quality: 9)
+        end
+      end
 
-      gilded_rose.tick
+      context "on sell date" do
+        it "reduces the days remaining more and quality twice as fast" do
+          gilded_rose = GildedRose.new(name: "Normal Item", days_remaining: 0, quality: 10)
+          gilded_rose.tick
+          expect(gilded_rose).to have_attributes(days_remaining: -1, quality: 8)
+        end
+      end
 
-      expect(gilded_rose).to have_attributes(days_remaining: 4, quality: 9)
+      context "after sell date" do
+        it "reduces quality twice as fast" do
+          gilded_rose = GildedRose.new(name: "Normal Item", days_remaining: -10, quality: 10)
+          gilded_rose.tick
+          expect(gilded_rose).to have_attributes(days_remaining: -11, quality: 8)
+        end
+      end
+
+      context "of zero quality" do
+        it "does not reduce quality" do
+          gilded_rose = GildedRose.new(name: "Normal Item", days_remaining: 5, quality: 0)
+          gilded_rose.tick
+          expect(gilded_rose).to have_attributes(days_remaining: 4, quality: 0)
+        end
+      end
     end
 
-    it "on sell date" do
-      gilded_rose = GildedRose.new(name: "Normal Item", days_remaining: 0, quality: 10)
+    context "Aged Brie" do
+      context "before sell date" do
+        it "increases quality and reduces days remaining" do
+          gilded_rose = GildedRose.new(name: "Aged Brie", days_remaining: 5, quality: 10)
+          gilded_rose.tick
+          expect(gilded_rose).to have_attributes(days_remaining: 4, quality: 11)
+        end
+      end
 
-      gilded_rose.tick
+      context "on sell date" do
+        it "increases quality by 2 and reduces days remaining" do
+          gilded_rose = GildedRose.new(name: "Aged Brie", days_remaining: 0, quality: 10)
+          gilded_rose.tick
+          expect(gilded_rose).to have_attributes(days_remaining: -1, quality: 12)
+        end
+      end
 
-      expect(gilded_rose).to have_attributes(days_remaining: -1, quality: 8)
+      context "after sell date" do
+        it "increases quality by 2 and reduces days remaining" do
+          gilded_rose = GildedRose.new(name: "Aged Brie", days_remaining: -10, quality: 10)
+          gilded_rose.tick
+          expect(gilded_rose).to have_attributes(days_remaining: -11, quality: 12)
+        end
+      end
     end
 
-    it "after sell date" do
-      gilded_rose = GildedRose.new(name: "Normal Item", days_remaining: -10, quality: 10)
+    context "Sulfuras" do
+      context "before sell date" do
+        it "does not change days remaining or quality" do
+          gilded_rose = GildedRose.new(name: "Sulfuras, Hand of Ragnaros", days_remaining: 5, quality: 80)
+          gilded_rose.tick
+          expect(gilded_rose).to have_attributes(days_remaining: 5, quality: 80)
+        end
+      end
 
-      gilded_rose.tick
+      context "on sell date" do
+        it "does not change days remaining or quality" do
+          gilded_rose = GildedRose.new(name: "Sulfuras, Hand of Ragnaros", days_remaining: 0, quality: 80)
+          gilded_rose.tick
+          expect(gilded_rose).to have_attributes(days_remaining: 0, quality: 80)
+        end
+      end
 
-      expect(gilded_rose).to have_attributes(days_remaining: -11, quality: 8)
+      context "after sell date" do
+        it "does not change days remaining or quality" do
+          gilded_rose = GildedRose.new(name: "Sulfuras, Hand of Ragnaros", days_remaining: -10, quality: 80)
+          gilded_rose.tick
+          expect(gilded_rose).to have_attributes(days_remaining: -10, quality: 80)
+        end
+      end
     end
 
-    it "of zero quality" do
-      gilded_rose = GildedRose.new(name: "Normal Item", days_remaining: 5, quality: 0)
+    context "Backstage Pass" do
+      context "long before sell date (more than 10 days)" do
+        it "increases quality by 1 and reduces days remaining" do
+          gilded_rose = GildedRose.new(name: "Backstage passes to a TAFKAL80ETC concert", days_remaining: 11, quality: 10)
+          gilded_rose.tick
+          expect(gilded_rose).to have_attributes(days_remaining: 10, quality: 11)
+        end
+      end
 
-      gilded_rose.tick
+      context "medium close to sell date (6 to 10 days)" do
+        it "increases quality by 2 and reduces days remaining" do
+          gilded_rose = GildedRose.new(name: "Backstage passes to a TAFKAL80ETC concert", days_remaining: 10, quality: 10)
+          gilded_rose.tick
+          expect(gilded_rose).to have_attributes(days_remaining: 9, quality: 12)
+        end
+      end
 
-      expect(gilded_rose).to have_attributes(days_remaining: 4, quality: 0)
-    end
-  end
+      context "very close to sell date (5 days or less)" do
+        it "increases quality by 3 and reduces days remaining" do
+          gilded_rose = GildedRose.new(name: "Backstage passes to a TAFKAL80ETC concert", days_remaining: 5, quality: 10)
+          gilded_rose.tick
+          expect(gilded_rose).to have_attributes(days_remaining: 4, quality: 13)
+        end
+      end
 
-  context "Aged Brie" do
-    it "before sell date" do
-      gilded_rose = GildedRose.new(name: "Aged Brie", days_remaining: 5, quality: 10)
+      context "on sell date" do
+        it "drops quality to 0" do
+          gilded_rose = GildedRose.new(name: "Backstage passes to a TAFKAL80ETC concert", days_remaining: 0, quality: 10)
+          gilded_rose.tick
+          expect(gilded_rose).to have_attributes(days_remaining: -1, quality: 0)
+        end
+      end
 
-      gilded_rose.tick
-
-      expect(gilded_rose).to have_attributes(days_remaining: 4, quality: 11)
-    end
-
-    it "with max quality" do
-      gilded_rose = GildedRose.new(name: "Aged Brie", days_remaining: 5, quality: 50)
-
-      gilded_rose.tick
-
-      expect(gilded_rose).to have_attributes(days_remaining: 4, quality: 50)
-    end
-
-    it "on sell date" do
-      gilded_rose = GildedRose.new(name: "Aged Brie", days_remaining: 0, quality: 10)
-
-      gilded_rose.tick
-
-      expect(gilded_rose).to have_attributes(days_remaining: -1, quality: 12)
-    end
-
-    it "on sell date near max quality" do
-      gilded_rose = GildedRose.new(name: "Aged Brie", days_remaining: 0, quality: 49)
-
-      gilded_rose.tick
-
-      expect(gilded_rose).to have_attributes(days_remaining: -1, quality: 50)
-    end
-
-    it "on sell date with max quality" do
-      gilded_rose = GildedRose.new(name: "Aged Brie", days_remaining: 0, quality: 50)
-
-      gilded_rose.tick
-
-      expect(gilded_rose).to have_attributes(days_remaining: -1, quality: 50)
+      context "after sell date" do
+        it "drops quality to 0" do
+          gilded_rose = GildedRose.new(name: "Backstage passes to a TAFKAL80ETC concert", days_remaining: -10, quality: 10)
+          gilded_rose.tick
+          expect(gilded_rose).to have_attributes(days_remaining: -11, quality: 0)
+        end
+      end
     end
 
-    it "after sell date" do
-      gilded_rose = GildedRose.new(name: "Aged Brie", days_remaining: -10, quality: 10)
-
-      gilded_rose.tick
-
-      expect(gilded_rose).to have_attributes(days_remaining: -11, quality: 12)
-    end
-
-    it "after sell date with max quality" do
-      gilded_rose = GildedRose.new(name: "Aged Brie", days_remaining: -10, quality: 50)
-
-      gilded_rose.tick
-
-      expect(gilded_rose).to have_attributes(days_remaining: -11, quality: 50)
-    end
-  end
-
-  context "Sulfuras" do
-    it "before sell date" do
-      gilded_rose = GildedRose.new(name: "Sulfuras, Hand of Ragnaros", days_remaining: 5, quality: 80)
-
-      gilded_rose.tick
-
-      expect(gilded_rose).to have_attributes(days_remaining: 5, quality: 80)
-    end
-
-    it "on sell date" do
-      gilded_rose = GildedRose.new(name: "Sulfuras, Hand of Ragnaros", days_remaining: 0, quality: 80)
-
-      gilded_rose.tick
-
-      expect(gilded_rose).to have_attributes(days_remaining: 0, quality: 80)
-    end
-
-    it "after sell date" do
-      gilded_rose = GildedRose.new(name: "Sulfuras, Hand of Ragnaros", days_remaining: -10, quality: 80)
-
-      gilded_rose.tick
-
-      expect(gilded_rose).to have_attributes(days_remaining: -10, quality: 80)
-    end
-  end
-
-  context "Backstage Pass" do
-    it "long before sell date" do
-      gilded_rose = GildedRose.new(name: "Backstage passes to a TAFKAL80ETC concert", days_remaining: 11, quality: 10)
-
-      gilded_rose.tick
-
-      expect(gilded_rose).to have_attributes(days_remaining: 10, quality: 11)
-    end
-
-    it "long before sell date at max quality" do
-      gilded_rose = GildedRose.new(name: "Backstage passes to a TAFKAL80ETC concert", days_remaining: 11, quality: 50)
-
-      gilded_rose.tick
-
-      expect(gilded_rose).to have_attributes(days_remaining: 10, quality: 50)
-    end
-
-    it "medium close to sell date upper bound" do
-      gilded_rose = GildedRose.new(name: "Backstage passes to a TAFKAL80ETC concert", days_remaining: 10, quality: 10)
-
-      gilded_rose.tick
-
-      expect(gilded_rose).to have_attributes(days_remaining: 9, quality: 12)
-    end
-
-    it "medium close to sell date upper bound at max quality" do
-      gilded_rose = GildedRose.new(name: "Backstage passes to a TAFKAL80ETC concert", days_remaining: 10, quality: 50)
-
-      gilded_rose.tick
-
-      expect(gilded_rose).to have_attributes(days_remaining: 9, quality: 50)
-    end
-
-    it "medium close to sell date lower bound" do
-      gilded_rose = GildedRose.new(name: "Backstage passes to a TAFKAL80ETC concert", days_remaining: 6, quality: 10)
-
-      gilded_rose.tick
-
-      expect(gilded_rose).to have_attributes(days_remaining: 5, quality: 12)
-    end
-
-    it "medium close to sell date lower bound at max quality" do
-      gilded_rose = GildedRose.new(name: "Backstage passes to a TAFKAL80ETC concert", days_remaining: 6, quality: 50)
-
-      gilded_rose.tick
-
-      expect(gilded_rose).to have_attributes(days_remaining: 5, quality: 50)
-    end
-
-    it "very close to sell date upper bound" do
-      gilded_rose = GildedRose.new(name: "Backstage passes to a TAFKAL80ETC concert", days_remaining: 5, quality: 10)
-
-      gilded_rose.tick
-
-      expect(gilded_rose).to have_attributes(days_remaining: 4, quality: 13)
-    end
-
-    it "very close to sell date upper bound at max quality" do
-      gilded_rose = GildedRose.new(name: "Backstage passes to a TAFKAL80ETC concert", days_remaining: 5, quality: 50)
-
-      gilded_rose.tick
-
-      expect(gilded_rose).to have_attributes(days_remaining: 4, quality: 50)
-    end
-
-    it "very close to sell date lower bound" do
-      gilded_rose = GildedRose.new(name: "Backstage passes to a TAFKAL80ETC concert", days_remaining: 1, quality: 10)
-
-      gilded_rose.tick
-
-      expect(gilded_rose).to have_attributes(days_remaining: 0, quality: 13)
-    end
-
-    it "very close to sell date lower bound at max quality" do
-      gilded_rose = GildedRose.new(name: "Backstage passes to a TAFKAL80ETC concert", days_remaining: 1, quality: 50)
-
-      gilded_rose.tick
-
-      expect(gilded_rose).to have_attributes(days_remaining: 0, quality: 50)
-    end
-
-    it "on sell date" do
-      gilded_rose = GildedRose.new(name: "Backstage passes to a TAFKAL80ETC concert", days_remaining: 0, quality: 10)
-
-      gilded_rose.tick
-
-      expect(gilded_rose).to have_attributes(days_remaining: -1, quality: 0)
-    end
-
-    it "after sell date" do
-      gilded_rose = GildedRose.new(name: "Backstage passes to a TAFKAL80ETC concert", days_remaining: -10, quality: 10)
-
-      gilded_rose.tick
-
-      expect(gilded_rose).to have_attributes(days_remaining: -11, quality: 0)
-    end
-  end
-
-  context "Conjured Mana" do
-    xit "before sell date" do
-      gilded_rose = GildedRose.new(name: "Conjured Mana Cake", days_remaining: 5, quality: 10)
-
-      gilded_rose.tick
-
-      expect(gilded_rose).to have_attributes(days_remaining: 4, quality: 8)
-    end
-
-    xit "before sell date at zero quality" do
-      gilded_rose = GildedRose.new(name: "Conjured Mana Cake", days_remaining: 5, quality: 0)
-
-      gilded_rose.tick
-
-      expect(gilded_rose).to have_attributes(days_remaining: 4, quality: 0)
-    end
-
-    xit "on sell date" do
-      gilded_rose = GildedRose.new(name: "Conjured Mana Cake", days_remaining: 0, quality: 10)
-
-      gilded_rose.tick
-
-      expect(gilded_rose).to have_attributes(days_remaining: -1, quality: 6)
-    end
-
-    xit "on sell date at zero quality" do
-      gilded_rose = GildedRose.new(name: "Conjured Mana Cake", days_remaining: 0, quality: 0)
-
-      gilded_rose.tick
-
-      expect(gilded_rose).to have_attributes(days_remaining: -1, quality: 0)
-    end
-
-    xit "after sell date" do
-      gilded_rose = GildedRose.new(name: "Conjured Mana Cake", days_remaining: -10, quality: 10)
-
-      gilded_rose.tick
-
-      expect(gilded_rose).to have_attributes(days_remaining: -11, quality: 6)
-    end
-
-    xit "after sell date at zero quality" do
-      gilded_rose = GildedRose.new(name: "Conjured Mana Cake", days_remaining: -10, quality: 0)
-
-      gilded_rose.tick
-
-      expect(gilded_rose).to have_attributes(days_remaining: -11, quality: 0)
+    context "Conjured Mana" do
+      context "before sell date" do
+        it "reduces quality by 2 and reduces days remaining" do
+          gilded_rose = GildedRose.new(name: "Conjured Mana Cake", days_remaining: 5, quality: 10)
+          gilded_rose.tick
+          expect(gilded_rose).to have_attributes(days_remaining: 4, quality: 8)
+        end
+      end
+
+      context "on sell date" do
+        it "reduces quality by 4 and reduces days remaining" do
+          gilded_rose = GildedRose.new(name: "Conjured Mana Cake", days_remaining: 0, quality: 10)
+          gilded_rose.tick
+          expect(gilded_rose).to have_attributes(days_remaining: -1, quality: 6)
+        end
+      end
+
+      context "after sell date" do
+        it "reduces quality by 4 and reduces days remaining" do
+          gilded_rose = GildedRose.new(name: "Conjured Mana Cake", days_remaining: -10, quality: 10)
+          gilded_rose.tick
+          expect(gilded_rose).to have_attributes(days_remaining: -11, quality: 6)
+        end
+      end
     end
   end
 end
